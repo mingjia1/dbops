3364b8ec05f4a4b5608dac14366151cf760f67ba
jack <jack@happycfc.com>
2026-06-07 10:41:48 +0800

    chore(web): remove dead Redux store + instanceSlice (no component uses it)

--- web-console/src/main.tsx
+++ web-console/src/main.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import App from './App'
-import { store } from './store'
 import { appleTheme } from './appTheme'
 import './index.css'
 import './styles/apple.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ConfigProvider locale={zhCN} theme={appleTheme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ConfigProvider>
-    </Provider>
+    <ConfigProvider locale={zhCN} theme={appleTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
   </React.StrictMode>,
 )
