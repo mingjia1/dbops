ecf821972976d5572861b3629f33f25042b5f9a3
jack <834189039@qq.com>
2026-06-23 10:18:55 +0800

    fix(M4): add typed PluginParams struct with ToMap() converter

--- platform-backend/internal/plugins/plugin.go
+++ platform-backend/internal/plugins/plugin.go
@@ -54,6 +54,44 @@ type PluginResult struct {
 	Warnings []string               `json:"warnings,omitempty"`
 }
 
+type PluginParams struct {
+	DeployMode    string            `json:"deploy_mode,omitempty"`
+	Flavor        string            `json:"flavor,omitempty"`
+	Version       string            `json:"version,omitempty"`
+	ServerID      int               `json:"server_id,omitempty"`
+	GTIDDomainID  int               `json:"gtid_domain_id,omitempty"`
+	VIP           string            `json:"vip,omitempty"`
+	VIPInterface  string            `json:"vip_interface,omitempty"`
+	ProxyHost     string            `json:"proxy_host,omitempty"`
+	ProxyPort     int               `json:"proxy_port,omitempty"`
+	GroupName     string            `json:"group_name,omitempty"`
+	LocalAddress  string            `json:"local_address,omitempty"`
+	GroupSeeds    []string          `json:"group_seeds,omitempty"`
+	Bootstrap     bool              `json:"bootstrap,omitempty"`
+	Custom        map[string]string `json:"custom,omitempty"`
+}
+
+func (p *PluginParams) ToMap() map[string]interface{} {
+	m := make(map[string]interface{})
+	if p.DeployMode != "" { m["deploy_mode"] = p.DeployMode }
+	if p.Flavor != "" { m["flavor"] = p.Flavor }
+	if p.Version != "" { m["version"] = p.Version }
+	if p.ServerID != 0 { m["server_id"] = p.ServerID }
+	if p.GTIDDomainID != 0 { m["gtid_domain_id"] = p.GTIDDomainID }
+	if p.VIP != "" { m["vip"] = p.VIP }
+	if p.VIPInterface != "" { m["vip_interface"] = p.VIPInterface }
+	if p.ProxyHost != "" { m["proxy_host"] = p.ProxyHost }
+	if p.ProxyPort != 0 { m["proxy_port"] = p.ProxyPort }
+	if p.GroupName != "" { m["group_name"] = p.GroupName }
+	if p.LocalAddress != "" { m["local_address"] = p.LocalAddress }
+	if len(p.GroupSeeds) > 0 { m["group_seeds"] = p.GroupSeeds }
+	if p.Bootstrap { m["bootstrap"] = p.Bootstrap }
+	if len(p.Custom) > 0 {
+		for k, v := range p.Custom { m[k] = v }
+	}
+	return m
+}
+
 type DefaultPluginMethods struct{}
 
 func (d *DefaultPluginMethods) Join(_ context.Context, _ PluginEnv, _ PluginNode) error {
