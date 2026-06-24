fe19f60b83a640d68637cc31fbe88a0603d4ce21
jack <834189039@qq.com>
2026-06-24 08:13:42 +0800

    fix: use mysql_native_password for root@% to enable TCP auth for MGR recovery channel

--- agent/internal/executor/task_executor.go
+++ agent/internal/executor/task_executor.go
@@ -835,9 +835,9 @@ func (e *TaskExecutor) deploySingleInstance(ctx context.Context, req DeployTaskR
 		}
 		socketPath := filepath.Join(dataDir, "mysql.sock")
 		retrySQL := fmt.Sprintf(
-			"CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s'; "+
-				"ALTER USER '%s'@'%%' IDENTIFIED BY '%s'; "+
-				"ALTER USER '%s'@'localhost' IDENTIFIED BY '%s'; "+
+			"CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED WITH mysql_native_password BY '%s'; "+
+				"ALTER USER '%s'@'%%' IDENTIFIED WITH mysql_native_password BY '%s'; "+
+				"ALTER USER '%s'@'localhost' IDENTIFIED WITH mysql_native_password BY '%s'; "+
 				"GRANT ALL PRIVILEGES ON *.* TO '%s'@'%%' WITH GRANT OPTION; FLUSH PRIVILEGES;",
 			escapeSQL(mysqlUser), escapeSQL(mysqlPass),
 			escapeSQL(mysqlUser), escapeSQL(mysqlPass),
